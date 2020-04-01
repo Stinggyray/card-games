dce29ca50a3757dd457155c4e2ca003d553eec48
Stinggyray <stinggyray@gmail.com>
2020-04-01 06:51:42 -0500

    made changes.. hopefully fixed something

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 8080
 
-app.use(express.static('public'))
-app.listen(port, () => console.log('Express server running on port ' + port))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', function(req, res){
-	res.render('index.html');
-});
+app.listen(port, () => console.log('Express server running on port ' + port))
